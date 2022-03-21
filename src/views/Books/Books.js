eb024c98e39c53459c8a96fd534d5e6c39f9a896
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-21 12:53:38 -0700

    wrote fetch books, tested connection with console.log

--- src/views/Books/Books.js
+++ src/views/Books/Books.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchBooks } from '../../services/books';
 
 function Books() {
+  const params = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchBooks();
+      console.log(data);
+    };
+    fetchData();
+  }, [params.name]);
+
   return <div>Books</div>;
 }
 
