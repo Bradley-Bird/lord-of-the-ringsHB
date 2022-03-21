744d358e72f5a58ba524a523c3c4261be982636b
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-21 12:30:51 -0700

    all characters grabbed on characters route clicked

--- src/views/Characters/Characters.js
+++ src/views/Characters/Characters.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { fetchCharacters } from '../../services/characters';
+import { useParams } from 'react-router-dom';
 
 function Characters() {
+  const [characters, setCharacters] = useState([]);
+  const params = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchCharacters('All');
+      console.log(data);
+      setCharacters(data);
+    };
+    fetchData();
+  }, [params.name]);
+
   return <div>Characters</div>;
 }
 
--- src/views/Films/Films.js
+++ src/views/Films/Films.js
@@ -8,7 +8,6 @@ function Films() {
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchFilms();
-      console.log(data);
       setFilms(data);
     };
     fetchData();
