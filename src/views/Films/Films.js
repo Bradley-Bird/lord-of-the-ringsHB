f23a270a124b3f87c63af54c8eb13efe12ca7888
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-21 12:13:31 -0700

    films object console.logged when clicking films path

--- src/views/Films/Films.js
+++ src/views/Films/Films.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { fetchFilms } from '../../services/films';
+import { useParams } from 'react-router-dom';
 
 function Films() {
+  const params = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchFilms();
+      console.log(data);
+    };
+    fetchData();
+  }, [params.name]);
+
   return <div>Films</div>;
 }
 
--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Main() {
-  return <div>Home</div>;
+  return <div>Welcome! Check out the links to learn all about Lord of The Rings!</div>;
 }
 
 export default Main;
