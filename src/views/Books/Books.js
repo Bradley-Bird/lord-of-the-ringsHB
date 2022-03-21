9d4c8b81fa7a325f215fa3735e77d9324edc8e93
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-21 12:56:04 -0700

    books now displaying in view on books route

--- src/views/Books/Books.js
+++ src/views/Books/Books.js
@@ -3,16 +3,24 @@ import { useParams } from 'react-router-dom';
 import { fetchBooks } from '../../services/books';
 
 function Books() {
+  const [books, setBooks] = useState([]);
   const params = useParams();
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchBooks();
       console.log(data);
+      setBooks(data);
     };
     fetchData();
   }, [params.name]);
 
-  return <div>Books</div>;
+  return (
+    <div>
+      {books.map((book) => (
+        <p key={book.id}>{book.title}</p>
+      ))}
+    </div>
+  );
 }
 
 export default Books;
