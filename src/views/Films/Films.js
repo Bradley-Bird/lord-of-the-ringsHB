d7be8c0966d2d0087d8470488e0f8c37537d4328
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-21 12:21:56 -0700

    film titles showing on films page

--- src/views/Films/Films.js
+++ src/views/Films/Films.js
@@ -1,18 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchFilms } from '../../services/films';
 import { useParams } from 'react-router-dom';
 
 function Films() {
+  const [films, setFilms] = useState([]);
   const params = useParams();
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchFilms();
       console.log(data);
+      setFilms(data);
     };
     fetchData();
   }, [params.name]);
 
-  return <div>Films</div>;
+  return (
+    <div>
+      {films.map((film) => (
+        <p key={film.id}>{film.title}</p>
+      ))}
+    </div>
+  );
 }
 
 export default Films;
