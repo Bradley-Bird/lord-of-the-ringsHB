3428eeab4420a65359dba93038e7cd1b198d19d1
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-21 17:44:40 -0700

    test passing

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  render(<App />);
+  const linkElement = screen.getByText(
+    /Welcome! Check out the links to learn all about Lord of The Rings!/i
+  );
+  expect(linkElement).toBeInTheDocument();
 });
